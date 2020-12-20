47d6c189042e42a1d9d46cc8e978998e6152ad2c
sailu <sailu@LAPTOP-2NGPENU5.hsd1.mi.comcast.net>
2020-12-19 21:19:54 -0500

    Initial commit

---
+++ spring-boot-rest1/src/main/java/com/devglan/SpringBootRest1Application.java
@@ -0,0 +1,14 @@
+//https://www.devglan.com/react-js/spring-boot-reactjs-crud-example
+package com.devglan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRest1Application.class, args);
+	}
+
+}
